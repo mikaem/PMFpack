af184408aba94079c91c84f7140fd1ecb9640ac5
Mikael Mortensen <mikael.mortensen@gmail.com>
2013-05-01 18:39:00 +0200

    Added Copyright stuff for Fortran files

--- fortran/MATHUTIL.f
+++ fortran/MATHUTIL.f
@@ -1,5 +1,21 @@
-  
-
+C Copyright (C) 2013 Ahmad El Sayed
+C 
+C  This file is part of PMFpack.
+C 
+C  PMFpack is free software: you can redistribute it and/or modify
+C  it under the terms of the GNU Lesser General Public License as published by
+C  the Free Software Foundation, either version 3 of the License, or
+C  (at your option) any later version.
+C 
+C  PMFpack is distributed in the hope that it will be useful,
+C  but WITHOUT ANY WARRANTY; without even the implied warranty of
+C  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+C  GNU Lesser General Public License for more details.
+C 
+C  You should have received a copy of the GNU Lesser General Public License
+C  along with PMFpack. If not, see <http://www.gnu.org/licenses/>.
+C 
+C 
       DOUBLE PRECISION FUNCTION ERFINV(Y)
 C     COMPUTES THE INVERSE OF THE ERROR FUNCTION
       IMPLICIT NONE 
